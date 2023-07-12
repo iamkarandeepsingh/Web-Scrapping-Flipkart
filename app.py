27d3a900af609f79f77c6de594931166f6f7c15e
iamkarandeepsingh <karandeeps2k@gmail.com>
2023-07-13 02:15:54 +0530

    first commit

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import requests
 from bs4 import BeautifulSoup as bs
 from urllib.request import urlopen as uReq
 import logging
+import pymongo
 logging.basicConfig(filename="scrapper.log" , level=logging.INFO)
 
 app = Flask(__name__)
@@ -73,6 +74,24 @@ def index():
                 reviews.append(mydict)
             logging.info("log my final result {}".format(reviews))
             return render_template('result.html', reviews=reviews[0:(len(reviews)-1)])
+            from pymongo.mongo_client import MongoClient
+
+            uri = "mongodb+srv://pwskills:pwskills@cluster0.tfjmcru.mongodb.net/?retryWrites=true&w=majority"
+
+            # Create a new client and connect to the server
+            client = MongoClient(uri)
+
+            # Send a ping to confirm a successful connection
+            try:
+                client.admin.command('ping')
+                print("Pinged your deployment. You successfully connected to MongoDB!")
+            except Exception as e:
+                print(e)
+
+            db = client['review_scrap']
+            review_col = db['review_scrap_data']
+            review_col.insert_many(reviews)
+            
         except Exception as e:
             logging.info(e)
             return 'something is wrong'
